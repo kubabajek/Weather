0c5c466ec2c90dd017a458b01604ae8deed68238
kubabajek <bajek@student.agh.edu.pl>
2023-06-12 13:35:00 +0200

    amendments

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Using free weather API returns days in next week where max temperature in Craow is less than passed in argument (default 10°C) and save it in to csv file.
+Using free weather API returns days in next week where max temperature in Craow is less than natural value passed in argument (default 10°C) and save it in to csv file.
--- weather.py
+++ weather.py
@@ -9,17 +9,29 @@ def int_type(arg_value):
         return int(arg_value)
     except ValueError:
         raise argparse.ArgumentTypeError(f"{arg_value} is not a valid integer")
-        
+
+
 def is_exist(filename):
     if os.path.exists(filename+'.csv'):
         raise argparse.ArgumentTypeError(f"File {filename}.csv already exists")
     return filename
         
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-t', action='store', dest='temp', default=10, type=int_type, help='int temp value to data filter (def. 10)')
-    parser.add_argument('-f', action='store', dest='filename', default='result',type=is_exist, help='csv file name without extension')
-    args = parser.parse_args()
+        
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', action='store', dest='temp', default=10, type=int_type, help='int temp value to data filter (def. 10)')
+parser.add_argument('-f', action='store', dest='filename', default='result',type=is_exist, help='csv file name without extension')
+args = parser.parse_args()
+
+
+def next_week_weather(highest_min_temp):
+    """Check in which days of next week in Cracow the minimal temperature is lees than passed in int argument
+
+    Args:
+        highest_min_temp (int): Minmal temperature above the days will be skipped
+
+    Returns:
+        list: List of lists contains date and minimal temperature
+    """
     
     api_key = "1cc9ea60b7d041e29b6220436231005"
     city = "Cracow"
@@ -32,9 +44,8 @@ def main():
     data = response.json()
     data = data['forecast']['forecastday']
 
-    check_temp = lambda day_data : day_data["day"]["mintemp_c"] < args.temp
+    check_temp = lambda day_data : day_data["day"]["mintemp_c"] < highest_min_temp
     tidy_data = lambda day_data : [day_data["date"],day_data["day"]["mintemp_c"]]
-    print_tidy = lambda tidy : print(f'Data: {tidy[0]}, TempMin: {tidy[1]}\u00b0C')
     
     data_filtered = list(filter(check_temp,data))
     data_tidied = list(map(tidy_data, data_filtered));
@@ -44,7 +55,15 @@ def main():
         writer.writerow(['data', 'temp'])
         writer.writerows(data_tidied)
     
-    list(map(print_tidy, data_tidied))
+    return data_tidied
+    
+    
+def main():
+    highest_min_temp = args.temp
+    days_temps = next_week_weather(highest_min_temp)
+    for days in days_temps:
+        print(f'Data: {days[0]}, TempMin: {days[1]}\u00b0C')
+    
     
 if __name__ == "__main__":
     main()
